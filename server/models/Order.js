7eaaa81c68304160c6312af76ca26e4c1323cecc
REZZZ-MC <rp261ch@gmail.com>
2024-11-10 07:03:23 +0700

    Create Order.js

---
+++ server/models/Order.js
@@ -0,0 +1,47 @@
+// server/models/Order.js
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
